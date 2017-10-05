d25095131184ef8b23b66f4fcfdfe1fd2e54aa7b
Aishwary Pramanik <aishwarypramanik@gmail.com>
2017-10-05 16:06:04 -0400

    Correct Anim0, started working on Anim1

--- CompAnim0/Assets/scripts/motion.cs
+++ CompAnim0/Assets/scripts/motion.cs
@@ -7,9 +7,11 @@ public class motion : MonoBehaviour {
     private float startTime;
     private float seekTime;
     public Text timer;
+    private float currentTime;
     // Use this for initialization
 	void Start () {
         startTime = Time.time;
+        currentTime = 0;
 	}
 	
 	// Update is called once per frame
@@ -22,7 +24,11 @@ public class motion : MonoBehaviour {
         {
             timer.text = "TIMER: "+seekTime.ToString("f0")+" second(s)";
             transform.position = new Vector3(5 * seekTime, 5 * seekTime, 0f);
-            transform.Rotate(0f, 18 * seekTime, 0f);
+            if (seekTime >= currentTime)
+            {
+                transform.Rotate(0f, 18, 0f);
+                currentTime += 1;
+            }
             Debug.Log("Time:" + (Time.time - startTime));
         }
 	}
